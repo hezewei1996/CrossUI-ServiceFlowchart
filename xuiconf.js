82b7f68d33bc7b954307dd501779611653a7ab5f
hezewei1996 <1219782682@qq.com>
2019-05-28 12:12:30 +0800

    Updated by CrossUI Web App Builder

--- xuiconf.js
+++ xuiconf.js
@@ -1,6 +1,23 @@
 
+
 // [[Page Appearance
-//xui.ini.$PageAppearance = {
-//    "theme":"webflat"
-//};
+xui.ini.$PageAppearance = {
+    "theme":"default"
+};
 // ]]Page Appearance
+
+
+// [[Font Icons CDN
+
+// ]]Font Icons CDN
+
+
+// [[Develop Env Setting
+xui.ini.$DevEnv = {
+    "designViewConf":{
+        "width":1366,
+        "height":768
+    },
+    "SpaceUnit":"px"
+};
+// ]]Develop Env Setting
